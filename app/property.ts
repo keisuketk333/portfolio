2f51399db43ca6bcdf1f8f3e2c4544fdda89320c
keisuketk333 <73272082+keisuketk333@users.noreply.github.com>
2024-04-20 02:40:00 +0000

    modify property

--- app/property.ts
+++ app/property.ts
@@ -1,4 +1,4 @@
 export const yourName = "Keisuke Tsukamoto";
-export const twitterLink = "https://twitter.com/"
-export const instagramLink = "https://instagram.com/"
-export const githubLink = "https://instagram.com/"
+export const twitterLink = "https://twitter.com/keisuketk333"
+export const instagramLink = "https://www.instagram.com/keisuke_64/"
+export const githubLink = "https://github.com/keisuketk333/"
